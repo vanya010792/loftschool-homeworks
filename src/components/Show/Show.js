befd8e1e8385bfd8977560a9b2f34b1e9fae2291
vanya010792 <vanaya010792@gmail.com>
2019-04-16 00:00:22 +0300

    up-test

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react'
-import apiInfo from '../../api'
+import { getShowInfo } from '../../api'
 import './Show.css'
 
 class Show extends Component {
     state = {
-        showId: this.props.showId,
-        data: false
+        showId: '',
+        data: []
     }
-    // componentDidMount() {
-    //     console.log(getShowInfo)
+    // static getDerivedStateFromProps( props, state ) {
+    //     if( props.showId.length > 0 ) {
+    //         console.log(props, state)
+    //     }
+    //     return null
+    // }
+    // componentDidUpdate() {
+    //     console.log(1)
     // }
 
     render() {
-        console.log(apiInfo[getShowInfo])
-        console.log(this.state.showId)
+        const propsShowId = this.props.showId
+        console.log(propsShowId)
+        // console.log('render', this.state.showId)
+        // console.log('data', this.state.data)
+        // if( this.props.showId ) {
+        //     this.setState({
+        //         showId: this.props.showId
+        //     })
+        // }
+        // console.log(this.state.showId)
         return(
             <React.Fragment>
                 {
-                    !!this.props.showId
+                    !!propsShowId
                     ? <div className="show">
                         <h1>Шоу выбрано { this.props.showId }</h1>
                       </div>
