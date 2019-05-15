1a7c3fee58b02487918b2bf217e23dbf8b3ebfd9
vanya010792 <vanya010792@gmail.com>
2019-05-15 14:45:07 +0300

    up

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -7,60 +7,47 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 // Обратите внимание на `orders.test.js`.
 // Он поможет понять, какие значения должен возвращать редьюсер.
 
-const moveOrderNext = pos => {
-  switch (pos) {
-    case 'clients':
-      return 'conveyor_1'
-    case 'conveyor_1':
-      return 'conveyor_2'
-    case 'conveyor_2':
-      return 'conveyor_3'
-    case 'conveyor_3':
-      return 'conveyor_4'
-    default:
-      return 'clients'
-  }
-}
-
 export default (state = [], action) => {
   console.log( action, state )
   switch (action.type) {
     case CREATE_NEW_ORDER:
       const { id, recipe } = action.payload
-      return {
+      return [
         ...state,
-        state: [
-          ...state,
-          {
+        {
             id: id,
-            ingredients: [],
+                ingredients: [],
             recipe: recipe,
             position: 'clients'
-          }
-        ]
-      }
+        }
+      ]
     case MOVE_ORDER_NEXT:
       const { position } = state[0]
       console.log( 'move-order', position )
-      // const position = state[payload - 1].position
-      return {
-        ...state,
-        position: [
-            ...state.position,
-          {
-            position: moveOrderNext( position )
-          }
-        ]
-      }
-    // case MOVE_ORDER_BACK:
-    //   const { id } = action.payload
-    //   const position = state[id - 1].position
-    //   return[
-    //     ...state,
-    //     {
-    //       position: this.moveOrderBack( position )
-    //     }
-    //   ]
+        switch ( position ) {
+            case 'clients':
+                return {
+                    state,
+                    position: 'conveyor_1'
+                }
+            case 'conveyor_1':
+                return {
+                    ...state,
+                    position: 'conveyor_2'
+                }
+            case 'conveyor_2':
+                return {
+                    ...state,
+                    position: 'conveyor_3'
+                }
+            case 'conveyor_3':
+                return {
+                    ...state,
+                    position: 'conveyor_4'
+                }
+            default:
+                return 'clients'
+        }
     case ADD_INGREDIENT:
       return[
         ...state
